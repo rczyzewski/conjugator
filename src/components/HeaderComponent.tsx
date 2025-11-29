a14e1f96e998d869f03b4294f39eee8e42c3fc70
Rafal Czyzewski <rafal.czyzewski@lottoland.com>
2025-11-29 09:58:43 +0100

    Updated HeaderComponent links to ensure correct query parameter formatting for navigation. Added new game options to the dropdown menu for enhanced user experience.

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -12,27 +12,23 @@ function HeaderComponent(): JSX.Element {
     return <header className="header">
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/">Conjugator</Navbar.Brand>
+                <Navbar.Brand href="/conjugator">Conjugator</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <NavDropdown title="Conjugacion Training" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/conjugator?/game/sw001">10 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw002">20 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw003">30 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw004">50 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw005">70 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw006">100 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw001">10 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw002">20 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw003">30 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw004">50 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw005">70 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw006">100 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw007">150 MostPopular</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/game/sw008">200 MostPopular</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/conjugator?/game/sw001">150 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw002">200 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw003">300 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw004">500 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw005">700 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/game/sw006">1000 MostPopular</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/conjugator?/list">Trained Conjugations</NavDropdown.Item>
-                            <NavDropdown.Item href="/conjugator?/setup">Game Settings</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/list">Trained Conjugations</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/all-verbs">All verbs</NavDropdown.Item>
+                            <NavDropdown.Item href="/conjugator/?/setup">Game Settings</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
