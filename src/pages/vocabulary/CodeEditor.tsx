7181663f9d2b14bd42cf1f01b7aa7347d9171b5a
Rafal Czyzewski <rafal.czyzewski@lottoland.com>
2025-12-19 07:41:10 +0100

    Remove unused localStorage keys retrieval in CodeEditor component

--- src/pages/vocabulary/CodeEditor.tsx
+++ src/pages/vocabulary/CodeEditor.tsx
@@ -165,7 +165,6 @@ export default function CodeEditor({ myMarkdown }: { myMarkdown: string }) {
 
   useEffect(() => {
     renderBook(code);
-    var keys = Object.keys(localStorage);
   }, []);
 
   return (
